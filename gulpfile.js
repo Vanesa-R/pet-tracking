618865497ecaf07113946a1efa21da02f2dc9534
Vanesa <uve.rdguez@gmail.com>
2023-03-25 19:19:16 +0100

    Inicializando proyecto

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+/* Genéricas */
+const { src, dest, series, parallel } = require('gulp');
+// const del = require("del");
+const plumber = require("gulp-plumber");
+
+/*Para tareas html */
+const pug = require("gulp-pug");
+const htmlmin = require("gulp-htmlmin");
+
+/*Para tareas  css */
+const sass = require("gulp-dart-sass");
+const autoprefixer = require ("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
+
+/* Para tareas js*/
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+
+/* Para assets */
+const imagemin = require("gulp-imagemin");
+
+
+
+// HTML
+const html = done => {
+    src("./app/html/pages/*.pug")
+    .pipe(plumber())
+    .pipe(pug({
+        doctype: "html",
+        pretty: true
+    }))
+    .pipe(htmlmin({
+        collapseWhitespace: true 
+    }))
+    .pipe(dest("dist"));
+    done()
+};
+
+
+
+// CSS
+const css = done => {
+    src("app/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(rename({
+        suffix: ".min",
+        extname: ".css"
+    }))
+    .pipe(dest("dist/css"));
+    done()
+};
+
+
+// JAVASCRIPT
+const js = done => {
+    src("app/js/*.js")
+    .pipe(plumber())
+    .pipe(concat("app.js"))
+    .pipe(babel())
+    .pipe(terser())
+    .pipe(dest("dist/js"));
+    done() 
+}
+
+// IMÁGENES
+const images = done => {
+    src("app/assets/**/*")
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(dest("dist/assets"))
+    done();
+}
+
+
+
+//Borrar ficheros no definitivos de la carpeta dist
+// gulp.task("clean", function(){
+//     return del("dist");
+// });
+
+
+exports.package = series(parallel(html, css, js), images)
